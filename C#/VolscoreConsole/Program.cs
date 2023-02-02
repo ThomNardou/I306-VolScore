755f34f613990a6550dc9d7619f5fef1473063bb
ThomNardou <thomas.nardou@eduvaud.ch>
2023-02-02 16:33:41 +0100

    Demander le choix de l'équipe pour voire les détails

--- C#/VolscoreConsole/Program.cs
+++ C#/VolscoreConsole/Program.cs
@@ -1,16 +1,46 @@
 ﻿using VolScore;
 
+//COnstantes
+const byte BYT_NBR_MATCH = 250;
+
+//Tableau 
+string[,] tab_match = new string[BYT_NBR_MATCH, 2];
+
+//Variables 
+char chrNuméroEquipe;
+char chrChoixEquipe = 'a';
+byte bytNumCaseTab = 0;
+int chrTemp;
+
 VolscoreDB vdb = new VolscoreDB();
+List<IVolscoreDB.Game> games = vdb.GetGames();
 List<IVolscoreDB.Team> teams = vdb.GetTeams();
 
-foreach (IVolscoreDB.Team team in teams)
+Console.WriteLine("---------------------------------------------------------------------------------");
+Console.WriteLine("-                               Liste de matchs                                 -");
+Console.WriteLine("---------------------------------------------------------------------------------\n");
+
+foreach (IVolscoreDB.Game game in games)
 {
-    Console.WriteLine(team.Name);
-    List<IVolscoreDB.Member> players = vdb.GetPlayers(team);
-    foreach (IVolscoreDB.Member member in players)
-    {
-        Console.WriteLine($"    {member.Number} {member.FirstName} {member.LastName}");
-    }
+    Console.Write((char)chrChoixEquipe + ". " + $"{game.ReceivingTeamName}    VS    {game.VisitingTeamName}\n");
+    chrTemp = chrChoixEquipe;
+    chrTemp = chrTemp - (chrTemp - 1);
+    tab_match[chrTemp, 0] = game.ReceivingTeamName;
+    tab_match[chrTemp, 1] = game.VisitingTeamName;
+    
+    chrChoixEquipe++;
 }
+
+Console.Write("\nCliquez sur le chiffre assigné à l'équipe pour voir les détails : ");
+chrNuméroEquipe = Convert.ToChar(Console.ReadKey().KeyChar);
+Console.WriteLine("\n");
+
+if (chrNuméroEquipe == 'a')
+{
+    Console.Clear();
+    Console.Write($"{games.Count}");
+    Console.WriteLine(games[0].VisitingTeamName);
+}
+
 
 Console.ReadKey();
